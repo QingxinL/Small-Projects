9dc24d4d1bb9b22b88ee2e06916f792047aa1734
Qingxin Li <qli1998@uw.edu>
2017-06-24 01:17:23 +0800

    first change

--- WebCrawler/Job_Info.py
+++ WebCrawler/Job_Info.py
@@ -18,10 +18,27 @@ def jobScraping(max_page):
         soup = BeautifulSoup(plain_text,'lxml')
         for link in soup.findAll('a',{'class':'mobileHide'}):
             href = link.get('href')
-            print(href)
-            title = link.string
-            print(title)
+            #print(href)
+            #title = link.string
+            #print(title)
+            get_requirement(href)
+            print()
         page+=1
 
 
+def get_requirement(single_url):
+    source_code = requests.get(single_url)
+    plain_text = source_code.text
+    soup = BeautifulSoup(plain_text, 'lxml')
+    for item in soup.findAll('font'):
+        require = item.string
+        print(require)
+
+# crawl the page in that page (recursive)
+    for link in soup.findAll('a'):
+        href = link.get('href')
+        print(href)
+
+
 jobScraping(1)
+#get_requirement('https://careers.twosigma.com/careers/DashJobDetail/New-York-New-York-United-States-Software-Engineering-Internship/2944')
